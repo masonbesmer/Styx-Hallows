7a457df73c7795535b1e39ea8d586d47a21b2489
masonbesmer <masonbesmer@gmail.com>
2022-08-06 21:58:22 -0500

    BirdPersonMovement.cs

---
+++ Assets/Scripts/BirdPersonMovement.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using console
+using UnityEngine.UI;
+
+public class BirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public float gravity = -9.81f/2f;
+    public float jumpHeight = 3f;
+    //public float elevationSpeed = 3f;
+    public float glideSpeed = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool gliding = false;
+    //bool flying = false;
+
+    Animator animator;
+
+    private float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        bool jump = Input.GetButtonDown("Jump");
+
+        //bool forward;
+        // if ((Input.GetAxisRaw("Vertical") != 0) || (Input.GetAxisRaw("Horizontal") != 0))
+        // {
+        //     forward = true;
+        // }
+        // else
+        // {
+        //     forward = false;
+        // }
+
+        //animator.SetBool("Forward", forward);
+
+
+        //check if the bird is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        /*
+        check for jump while jumping and not flying and start flying
+        if (!isGrounded && Input.GetButtonDown("Jump") && !flying)
+        {
+            StartFlying();
+        }
+        
+        //check if flying and descend
+        if (flying && Input.GetButton("Descend"))
+        {
+            //move controller down
+            controller.Move(Vector3.down * elevationSpeed * Time.deltaTime);
+        }
+
+        //check if flying and ascend
+        if (flying && Input.GetButton("Jump"))
+        {
+            //move controller up
+            controller.Move(Vector3.up * elevationSpeed * Time.deltaTime);
+        }
+
+        if (flying && isGrounded) 
+        {
+            StopFlying();
+        }
+        */
+ 
+        //Set velocity to -2 if grounded and not gliding
+        if (isGrounded && velocity.y < 0)
+        {
+            gliding = false;
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && !isGrounded && !gliding)
+        {
+            gliding = true;
+            velocity.y =  -glideSpeed;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            //animator.SetBool("Jump", jump);
+        }
+        else if(isGrounded)
+        {
+            //animator.SetBool("Jump", jump);
+        }
+
+        //Apply gravity and velocity of gravity to the Player
+        if (!gliding)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
+
+        //Let the Player jump if not grounded or flying
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    //Enable flying mode
+    // void StartFlying() {
+    //     flying = true;
+    //     velocity.y = 0;
+    //     controller.Move(velocity * Time.deltaTime);
+    //     //Start flying animation
+    //     gravity = 0;
+    //     //start bobbing
+    //     Debug.Log("Start Flying");
+    // }
+
+    // void StopFlying() {
+    //     flying = false;
+    //     //Stop flying animation
+    //     gravity = -19.62f;
+    //     //stop bobbing
+    //     Debug.Log("Stop Flying");
+    // }
+}
